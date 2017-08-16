54257a2c68c3a46d7eea1a485909862c69d4113e
NealCaf <NealCaf@users.noreply.github.com>
2017-08-16 09:36:41 +0800

    Changes to be committed: 	modified:   ShellSort.c

--- ShellSort.c
+++ ShellSort.c
@@ -1,12 +1,66 @@
 #include <stdio.h>
 
-void shellsort(int nums[], int len)
+void printnums(int nums[], int len)
 {
-	int i = 0, j = 0;
+	int i = 0;
+	printf("nums = ");
+	for(i=0;i<len;i++)
+	{
+		printf("%d, ", nums[i]);
+	}
+	printf("\n");
+}
+
+void shellsort1(int nums[], int len)
+{
+	int i = 0, j = 0, k = 0;
 	int step = 0;
+	int temp = 0;
 
 	for(step=len/2;step>0;step/=2)
 	{
-		for(
+		for(i=0;i<step;i++)
+		{
+			for(j=i+step;j<len;j+=step)
+			{
+				temp = nums[j];
+				for(k=j-step;k>=0&&nums[k]>temp;k-=step)
+				{
+					nums[k+step] = nums[k];
+				}
+				nums[k+step] = temp;
+			}
+		}
 	}
+}
+
+void shellsort2(int nums[], int len)
+{
+	int i = 0, j = 0, k = 0;
+	int step = 0;
+	int temp = 0;
+
+	for(step=len/2;step>0;step/=2)
+	{
+		for(i=step;i<len;i++)
+		{
+			temp = nums[i];
+			for(k=i-step;k>=0&&nums[k]>temp;k-=step)
+			{
+				nums[k+step] = nums[k];
+			}
+			nums[k+step] = temp;
+		}
+	}
+}
+
+int main()
+{
+	int nums[] = {9, -2, 2, 0, 8, 4, 5, -3, 1};
+	int len = 0;
+
+	len = sizeof(nums) / sizeof(int);
+	printf("len = %d\n", len);
+	shellsort2(nums, len);
+	printnums(nums, len);
 }
